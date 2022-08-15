4daea773c97dfb29bfabaf2c550cb8f677df41d2
Lev <fedorichev16@gmail.com>
2022-08-15 21:47:29 +0300

    add try/catch

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,26 +6,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    public static void main(String[] args){
+        try (
+                ServerSocket serverSocket = new ServerSocket(8080);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            System.out.println("New connection accepted");
 
-        System.out.println("New connection accepted");
+            out.println("Write your name:");
 
-        out.println("Write your name:");
+            final String name = in.readLine();
 
-        final String name = in.readLine();
+            out.println("Are you child? (yes/no)");
 
-        out.println("Are you child? (yes/no)");
+            final String answer = in.readLine();
 
-        final String answer = in.readLine();
-
-        if (answer.equals("yes")) {
-            out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
-        } else if (answer.equals("no")) {
-            out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+            if (answer.equals("yes")) {
+                out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+            } else if (answer.equals("no")) {
+                out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+            }
+        } catch (IOException e) {
+            System.out.println("Error");
         }
     }
 }
